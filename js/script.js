ca698b9f915ac39c2628841f8dc4048604c931f1
BRICHE Nicolas <nicolas.briche11@gmail.com>
2023-05-17 13:29:13 +0200

    Ajout js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// alert(screen.width);
+
+document.addEventListener("DOMContentLoaded", function () {
+    var burgerIcon = document.getElementById("header--burger-checkbox");
+    var menu = document.getElementById("header--nav");
+    var main = document.getElementsByTagName("main")[0];
+    var menuItem = document.getElementsByClassName("header--nav-item");
+
+    burgerIcon.onclick = function () {
+        if (burgerIcon.checked) {
+            menu.style.setProperty("display", "block");
+            // On bloque le scroll
+            document.getElementsByTagName("html")[0].style.setProperty("overflow", "hidden");
+
+        } else {
+            menu.style.setProperty("display", "none");
+            // On active le scroll
+            document.getElementsByTagName("html")[0].style.setProperty("overflow", "visible");
+        }
+    }
+
+    // Détéction du click sur un item du menu smartphone
+    for (let i = 0; i < menuItem.length; i++) {
+        const element = menuItem[i];
+        if (window.innerWidth < 1000) {
+            element.onclick = function () {
+                menu.style.setProperty("display", "none");
+                main.style.setProperty("filter", "none");
+                burgerIcon.checked = false;
+                // On active le scroll
+                document.getElementsByTagName("html")[0].style.setProperty("overflow", "visible");
+            }
+        }
+    }
+});
